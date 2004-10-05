765fbd1a1d0f801176fe1479542185bc0fc45103
dvbmatt <dvbmatt>
2004-10-05 18:23:56 +0000

    updated 05.10.2004_02b5

--- src/D2V.java
+++ src/D2V.java
@@ -27,94 +27,127 @@
 import java.io.*;
 import java.util.*;
 
-public class D2V {
+public class D2V
+{
 
-ArrayList d2v = new ArrayList();
-int[]  fps_table = { 0,23976,24000,25000,29970,30000,50000,59940,60000,0,0,0,0,0,0,0 };
-String[] type = { " 2"," 9" };
-String lastGOP = "", nextGOP = "";
-int entry=0, file=0, frame1=0, frame2=0;
-String d2vfile = "";
-String[] d2voptions = { "iDCT_Algorithm=2", "YUVRGB_Scale=1", "Luminance=128,0",
-		"Picture_Size=0,0,0,0,0,0", "Field_Operation=0" };
+	ArrayList d2v = new ArrayList();
 
-public void Init(String fparent) {
-	entry=0;
-	file=0;
-	d2v.clear();
-	d2vfile = fparent+".d2v";
-	d2v.add("DVD2AVIProjectFile");
-	d2v.add("1");
-	d2v.add(""+(fparent.length()+4)+" "+fparent+".mpv");
-	d2v.add("");
-	d2v.add("Stream_Type=0,0,0");
-	d2v.add(d2voptions[0]);
-	d2v.add(d2voptions[1]);
-	d2v.add(d2voptions[2]);
-	d2v.add(d2voptions[3]);
-	d2v.add(d2voptions[4]);
-	entry+=9;
-}
+	int[]  fps_table = { 0,23976,24000,25000,29970,30000,50000,59940,60000,0,0,0,0,0,0,0 };
+	int entry=0, file=0, frame1=0, frame2=0;
 
-public int getPart() {
-	return file;
-}
+	String[] type = { " 2"," 9" };
+	String lastGOP = "", nextGOP = "";
+	String d2vfile = "";
 
-public void setFile(String fparent) {
-	d2v.add(3+file,""+fparent.length()+" "+fparent);
-	file++;
-	entry++;
-}
+	String[] d2voptions = { 
+		"iDCT_Algorithm=2", 
+		"YUVRGB_Scale=1", 
+		"Luminance=128,0",
+		"Picture_Size=0,0,0,0,0,0", 
+		"Field_Operation=0" 
+	};
 
-public String[] readOptions() {
-	return d2voptions;
-}
-
-public void setOptions(String[] newoptions) {
-	d2voptions = (String[])newoptions.clone();
-}
-
-public void FrameRate(byte framerate) {
-	d2v.add("Frame_Rate="+(fps_table[15&framerate]));
-	d2v.add("");
-	entry += 2;
-}
-
-public void addGOP(long pos, int frames) {
-	String fill = "7 "+file+" "+Integer.toHexString((int)(pos/2048L)).toUpperCase();
-	for (int a=0;a<frames;a++) 
-		fill += type[0];
-	lastGOP = fill+type[0]+type[0]+type[1];
-	d2v.add(fill);
-	entry++;
-}
-
-public void write(long pos, long frame3) {
-	d2v.set(1,""+(file+1));
-	d2v.add(11+file,"Location=0,0,"+file+","+Integer.toHexString((int)(pos/2048L)).toUpperCase());
-	entry++;
-	d2v.set(entry,lastGOP);
-	d2v.add("");
-	d2v.add("FINISHED");
-	if (d2v.size()<14) { 
-		d2v.clear(); 
-		return; 
-	}
-
-	try 
+	public void Init(String fparent)
 	{
-	BufferedWriter d2vout = new BufferedWriter(new FileWriter(d2vfile));
-	for (int a=0;a<d2v.size();a++) {
-		d2vout.write(d2v.get(a).toString());
-		d2vout.newLine();
-	}
-	d2vout.close();
+		entry=0;
+		file=0;
 
+		d2v.clear();
+		d2vfile = fparent + ".d2v";
+		d2v.add("DVD2AVIProjectFile");
+		d2v.add("1");
+		d2v.add("" + (fparent.length() + 4) + " " + fparent + ".mpv");
+		d2v.add("");
+		d2v.add("Stream_Type=0,0,0");
+		d2v.add(d2voptions[0]);
+		d2v.add(d2voptions[1]);
+		d2v.add(d2voptions[2]);
+		d2v.add(d2voptions[3]);
+		d2v.add(d2voptions[4]);
+		entry += 9;
 	}
-	catch (IOException e) { 
-		X.Msg("-> d2v write error!"); 
-	}
-	d2v.clear();
-}
 
+	public int getPart()
+	{
+		return file;
+	}
+
+	public void setFile(String fparent)
+	{
+		d2v.add(3 + file, "" + fparent.length() + " " + fparent);
+		file++;
+		entry++;
+	}
+
+	public String[] readOptions()
+	{
+		return d2voptions;
+	}
+
+	public void setOptions(String[] newoptions)
+	{
+		d2voptions = (String[])newoptions.clone();
+	}
+
+	public void FrameRate(byte framerate)
+	{
+		d2v.add("Frame_Rate=" + (fps_table[15&framerate]));
+		d2v.add("");
+		entry += 2;
+	}
+
+	public void addGOP(long pos, int frames)
+	{
+		String fill = "7 " + file + " " + Integer.toHexString((int)(pos/2048L)).toUpperCase();
+
+		for (int a=0; a < frames; a++) 
+			fill += type[0];
+
+		lastGOP = fill + type[0] + type[0] + type[1];
+		d2v.add(fill);
+		entry++;
+	}
+
+	public void write(long pos, long frame3)
+	{
+		if (d2v.size() < 11)
+		{ 
+			d2v.clear(); 
+			return; 
+		}
+
+		d2v.set(1, "" + (file + 1));
+		d2v.add(11 + file,"Location=0,0," + file + "," + Integer.toHexString((int)(pos/2048L)).toUpperCase());
+
+		entry++;
+		d2v.set(entry,lastGOP);
+
+		d2v.add("");
+		d2v.add("FINISHED");
+
+		if (d2v.size() < 14)
+		{ 
+			d2v.clear(); 
+			return; 
+		}
+
+		try 
+		{
+			BufferedWriter d2vout = new BufferedWriter(new FileWriter(d2vfile));
+
+			for (int a=0; a < d2v.size(); a++)
+			{
+				d2vout.write(d2v.get(a).toString());
+				d2vout.newLine();
+			}
+
+			d2vout.close();
+		}
+		catch (IOException e)
+		{ 
+			X.Msg(Resource.getString("msg.log.error") + " (D2V) " + e); 
+		}
+
+		d2v.clear();
+	}
 }
