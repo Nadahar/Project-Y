a1a6f0403f3d0a092351530225e67de5e158845e
dvbmatt <dvbmatt>
2004-10-25 20:33:58 +0000

    ProjectX 081.9.00 25.10.04

---
+++ build.bat
@@ -0,0 +1,28 @@
+@echo on
+
+rem System dependend paths to the JDK
+set JAVA_HOME=C:\Programme\jdk14205
+set PATH=C:\Programme\jdk14205\bin
+
+del ProjectX.jar
+
+cd build
+
+del *.gif
+del *.properties
+del *.bin
+del *.tbl
+del edu\stanford\ejalbert\*.class
+del net\sourceforge\dvb\projectx\*.class
+
+cd..
+
+javac.exe -O -classpath src -d build @sources.lst
+
+pause
+
+copy src\resources\*.* build
+
+jar.exe cfvm ProjectX.jar MANIFEST.MF -C build .
+
+pause
