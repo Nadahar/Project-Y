9670863e2e9caa63ab7d198a4685f9b1150e04f2
dvbmatt <dvbmatt>
2004-10-28 19:40:59 +0000

    ProjectX 081.9.00 25.10.04

--- build.bat
+++ build.bat
@@ -7,16 +7,6 @@ set PATH=%JAVA_HOME%\bin
 del ProjectX.jar
 
 mkdir build
-cd build
-
-del *.gif
-del *.properties
-del *.bin
-del *.tbl
-del edu\stanford\ejalbert\*.class
-del net\sourceforge\dvb\projectx\*.class
-
-cd..
 
 javac.exe -O -classpath src -d build @sources.lst
 
@@ -25,5 +15,3 @@ pause
 copy resources\*.* build
 
 jar.exe cfvm ProjectX.jar MANIFEST.MF -C build .
-
-pause
